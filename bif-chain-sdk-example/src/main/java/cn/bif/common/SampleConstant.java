89326a623d46bc8a2fdff8bd4dbf13b6c57285e8
Caict-GuoShiJie <90613859+Caict-GuoShiJie@users.noreply.github.com>
2024-01-25 13:52:29 +0800

    update demo

--- bif-chain-sdk-example/src/main/java/cn/bif/common/SampleConstant.java
+++ bif-chain-sdk-example/src/main/java/cn/bif/common/SampleConstant.java
@@ -21,6 +21,6 @@ package cn.bif.common;
 
 public class SampleConstant {
 
-      public static final String SDK_INSTANCE_URL = "http://test-bif-core.xinghuo.space";
+      public static final String SDK_INSTANCE_URL = "http://test.bifcore.bitfactory.cn";
 
 }
